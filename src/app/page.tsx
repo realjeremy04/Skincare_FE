18c4d9282fa132605689e1f3f413838dc5d38c3c
realjeremy04 <tuankietvt04@gmail.com>
2025-03-17 03:27:09 +0700

    add loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,58 @@
-'use client';  // Mark this as a Client Component
-import { usePathname } from 'next/navigation';  
+// 'use client';  // Mark this as a Client Component
+// import { usePathname } from 'next/navigation';  
+// import { AppProps } from 'next/app';
+// import { JSX } from 'react';
+// import HomePage from './Homepage/HomePage';
+// import ContactPage from './contact/page';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   const pathname = usePathname();  // Get the current route path
+
+//   // Centralized Route Configuration
+//   const routes: { [key: string]: JSX.Element } = {
+//     '/': <HomePage />,
+//     //add more routes here
+//     '/contact': <ContactPage />,
+//   };
+
+//   // Check if the route exists, otherwise show NotFoundPage
+//   const CurrentPage = routes[pathname] ;
+
+//   return (
+//     <div>
+//       {CurrentPage}
+//     </div>
+//   );
+// }
+
+// export default MyApp;
+
+
+'use client';
+import { usePathname } from 'next/navigation';
 import { AppProps } from 'next/app';
-import { JSX } from 'react';
+
 import HomePage from './Homepage/HomePage';
 import ContactPage from './contact/page';
+import { AuthProvider } from '@/libs/context/AuthContext';
+import { JSX } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const pathname = usePathname();  // Get the current route path
+  const pathname = usePathname();
 
-  // Centralized Route Configuration
   const routes: { [key: string]: JSX.Element } = {
     '/': <HomePage />,
-    //add more routes here
     '/contact': <ContactPage />,
   };
 
-  // Check if the route exists, otherwise show NotFoundPage
-  const CurrentPage = routes[pathname] ;
+  const CurrentPage = routes[pathname] || <Component {...pageProps} />; // Fallback to default Component
 
   return (
-    <div>
-      {CurrentPage}
-    </div>
+    <AuthProvider>
+      <div>
+        {CurrentPage}
+      </div>
+    </AuthProvider>
   );
 }
 
